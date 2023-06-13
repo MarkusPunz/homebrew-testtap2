e9c763b3edfca8e77bedd8049c3e4b376223a3d1
Test Test <mp@Tests-iMac.local>
2023-06-13 11:38:33 +0200

    mptest2 0.0.6
    Closes #7.
    
    Signed-off-by: MarkusPunz <89575628+MarkusPunz@users.noreply.github.com>

--- Formula/mptest2.rb
+++ Formula/mptest2.rb
@@ -2,7 +2,7 @@ class Mptest2 < Formula
   desc "Test Mptest"
   homepage "https://www.bdc.at"
   url "https://github.com/MarkusPunz/testapp1/raw/main/test.tar.gz"
-  version "0.0.5"
+  version "0.0.6"
   sha256 "666f6626b4b2077369fdd6c134ea3666a2a136b6e9dfa2d7dcb0b1ea9fafa5c7"
 
   bottle do
