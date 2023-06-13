ac851cf5b335a8a8b87d4f1e23a41d395b5a3bee
Test Test <mp@Tests-iMac.local>
2023-06-13 08:41:39 +0200

    mptest 0.0.1 (new formula)

--- Formula/mptest2.rb
+++ Formula/mptest2.rb
@@ -2,7 +2,7 @@ class Mptest2 < Formula
   desc "Test Mptest"
   homepage "https://www.bdc.at"
   url "https://github.com/MarkusPunz/testapp1/raw/main/test.tar.gz"
-  version "0.0.3"
+  version "0.0.4"
   sha256 "666f6626b4b2077369fdd6c134ea3666a2a136b6e9dfa2d7dcb0b1ea9fafa5c7"
 
   on_macos do
