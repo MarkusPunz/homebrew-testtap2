d245523e022c802b9ceb3c9385398425afbdeb1e
Test Test <mp@Tests-iMac.local>
2023-06-13 12:44:58 +0200

    mptest 0.0.1 (new formula)

--- Formula/mptest2.rb
+++ Formula/mptest2.rb
@@ -2,7 +2,7 @@ class Mptest2 < Formula
   desc "Test Mptest"
   homepage "https://www.bdc.at"
   url "https://github.com/MarkusPunz/testapp1/raw/main/test.tar.gz"
-  version "0.0.8"
+  version "0.0.9"
   sha256 "666f6626b4b2077369fdd6c134ea3666a2a136b6e9dfa2d7dcb0b1ea9fafa5c7"
 
   bottle do
@@ -15,12 +15,6 @@ class Mptest2 < Formula
     def install
       prefix.install Dir["*"]
       lib.install_symlink prefix/"acosid-pkcs11.dylib"
-    end
-  end
-
-  on_linux do
-    def install
-      prefix.install Dir["*"]
     end
   end
 
