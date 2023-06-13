76601e3ec135adeca3860c3157f22a0af477c839
MarkusPunz <89575628+MarkusPunz@users.noreply.github.com>
2023-06-13 09:22:55 +0000

    mptest2: update 0.0.5 bottle.

--- Formula/mptest2.rb
+++ Formula/mptest2.rb
@@ -6,9 +6,9 @@ class Mptest2 < Formula
   sha256 "666f6626b4b2077369fdd6c134ea3666a2a136b6e9dfa2d7dcb0b1ea9fafa5c7"
 
   bottle do
-    root_url "https://github.com/MarkusPunz/homebrew-testtap2/releases/download/mptest2-0.0.4"
-    sha256 cellar: :any_skip_relocation, ventura:      "292120bd7f4e8d6fecca3b4ed151632fca601f64a92cd69c201b6cc1f033d6f3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "820d241f4eb45163dbe72014cce25eba47c3a678d3a7bd4f4bcde95408aab731"
+    root_url "https://github.com/MarkusPunz/homebrew-testtap2/releases/download/mptest2-0.0.5"
+    sha256 cellar: :any_skip_relocation, ventura:      "29e3e15ea7c20c79bc61abc724fcebad91e8cbb4fdda68067c2c537102f0b98e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b99cc5a90d67662e06f7944fe1c4fa1156ddc8b144a1bd2ecdc086859028c709"
   end
 
   on_macos do
